fc8def85a72438679ec23b7a9b7dafbb7ef7505e
LeslieChiang <wslchiang@gmail.com>
2022-01-24 13:03:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+            <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, Leslie.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (Deploy Test)
         </a>
+        <div>BaseURL: {config.baseURL}</div>
       </header>
     </div>
   );
